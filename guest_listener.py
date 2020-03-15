0bfa91fbcec66e89e16411e443d1deb2d8122ab0
Milan Herbig <milanherbig@gmail.com>
2020-03-15 14:35:41 +0100

    guest side implementation finished, now completely

--- guest_listener.py
+++ guest_listener.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
 import socket
+import evdev
 
 from key_enums import Keys
-from evdev import uinput, ecodes as e
 
 
-class KeySender(uinput.UInput):
+class KeySender(evdev.uinput.UInput):
 
     def __init__(self):
         super(KeySender, self).__init__()
@@ -19,22 +19,22 @@ class KeySender(uinput.UInput):
         }
 
     def __press_key(self, key_id):
-        print(F"Pressing key {key_id} down")
-        self.write(e.EV_KEY, key_id, 1)
-        self.write(e.EV_KEY, key_id, 0)
+        print(F"Sending key id {key_id} to uinput")
+        self.write(evdev.ecodes.EV_KEY, key_id, 1)
+        self.write(evdev.ecodes.EV_KEY, key_id, 0)
         self.syn()
 
     def press_playpause(self):
-        self.__press_key(e.KEY_PLAYPAUSE)
+        self.__press_key(evdev.ecodes.KEY_PLAYPAUSE)
 
     def press_stop(self):
-        self.__press_key(e.KEY_STOPCD)
+        self.__press_key(evdev.ecodes.KEY_STOPCD)
 
     def press_next(self):
-        self.__press_key(e.KEY_NEXTSONG)
+        self.__press_key(evdev.ecodes.KEY_NEXTSONG)
 
     def press_prev(self):
-        self.__press_key(e.KEY_PREVIOUSSONG)
+        self.__press_key(evdev.ecodes.KEY_PREVIOUSSONG)
 
     def press_key(self, enum):
         handler_method = self.key_mapping[enum]
@@ -47,34 +47,41 @@ class KeyListener:
         self.port = port
 
     def start_listening(self):
+        print(f"Started, will try to bind to {(self.host_ip, self.port)}...")
+
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.bind((self.host_ip, self.port))
             s.listen()
-            print(F"Binded and listening on {(HOST, PORT)}")
+
+            print(F"Binded and listening on {(self.host_ip, self.port)}")
 
             while True:
+                print("Waiting for connection")
+
                 conn, addr = s.accept()
                 with conn:
                     with KeySender() as key_sender:
-                        print('Connected by', addr)
+                        print('Connected client from', addr)
                         while True:
-                            data = conn.recv(2)
-                            if not data:
+                            data = conn.recv(64)
+                            if data:
+                                self.process_data(key_sender, data)
+                            else:
                                 break
-                            print("Received", data)
-                            key_sender.press_key(int(data))
-                print(addr, "disconnected")
+
+                print(addr, "Client disconnected")
+
+    @staticmethod
+    def process_data(key_sender, data):
+        data = data.decode()
+        for str_id in data:
+            print("Received key num id", str_id)
+            key_sender.press_key(int(str_id))
 
 
 if __name__ == '__main__':
-    HOST = '127.0.0.1'
+    HOST = '192.168.192.128'
     PORT = 65433
 
-    l = KeyListener(HOST, PORT)
-    l.listen()
-
-    # if __name__ == '__main__':
-    #     press_play_pause()
-    #     press_stop()
-    #     press_prev()
-    #     press_next()
+    incoming_listener = KeyListener(HOST, PORT)
+    incoming_listener.start_listening()
